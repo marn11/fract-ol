7cee20a8f1776f8a76c7664cca3cff9978a642f6
Marin Benchellal <yourintv.84@gmail.com>
2024-03-15 20:55:11 +0000

    test

--- src/scaling.c
+++ src/scaling.c
@@ -6,14 +6,14 @@
 /*   By: mbenchel <mbenchel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 02:38:08 by mbenchel          #+#    #+#             */
-/*   Updated: 2024/03/13 02:48:35 by mbenchel         ###   ########.fr       */
+/*   Updated: 2024/03/15 20:47:11 by mbenchel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
 
-double scaling(double unscaled, double new_min, double new_max, double old_min, double old_max)
+double scaling(double unscaled, double new_min, double new_max, double old_max)
 {
-	return (new_max - new_min) * (unscaled - old_min) / (old_max - old_min) + new_min;
+	return (new_max - new_min) * (unscaled)/ (old_max) + new_min;
 }
 // tldraw to understand
