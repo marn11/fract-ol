3fa469943f766a9bb10f297e4c2a258e9220b925
Marin Benchellal <yourintv.84@gmail.com>
2024-03-18 21:03:32 +0000

    x

--- src/init.c
+++ src/init.c
@@ -6,11 +6,7 @@
 /*   By: mbenchel <mbenchel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 01:35:04 by mbenchel          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/03/18 20:53:11 by mbenchel         ###   ########.fr       */
-=======
-/*   Updated: 2024/03/17 23:43:04 by mbenchel         ###   ########.fr       */
->>>>>>> 70d26bcaea7fe6b3f65bc429fdf1588ff28a26b5
+/*   Updated: 2024/03/18 21:03:24 by mbenchel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +14,7 @@
 
 void	values(t_fract *fractal)
 {
-<<<<<<< HEAD
 	fractal->iter_max = 42;
-=======
-	
->>>>>>> 70d26bcaea7fe6b3f65bc429fdf1588ff28a26b5
 }
 void	init(t_fract *fractal)
 {
