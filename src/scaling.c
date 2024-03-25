7e6c92a2763ebba6d54a5c14b72a955f101db564
Marin Benchellal <yourintv.84@gmail.com>
2024-03-25 19:55:23 +0000

    x

--- src/scaling.c
+++ src/scaling.c
@@ -6,7 +6,7 @@
 /*   By: mbenchel <mbenchel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 02:38:08 by mbenchel          #+#    #+#             */
-/*   Updated: 2024/03/25 19:55:11 by mbenchel         ###   ########.fr       */
+/*   Updated: 2024/03/25 19:55:14 by mbenchel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,4 +72,3 @@ void	pixel_check(int x, int y, t_fract *fractal)
 	}
 	my_pixel_put(&fractal->img, x, y, BLACK);
 }
-
